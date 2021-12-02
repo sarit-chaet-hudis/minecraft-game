765ead035b71d89f875a8cd6fed0cdaed793b67a
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-02 22:06:17 +0200

    background

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const MATRIX_HEIGHT = 20;
 
 const textures = {
   dirt: "dirt",
+  grass: "grass",
   sky: "sky",
   wood: "wood",
   leaves: "leaves",
@@ -13,9 +14,11 @@ function initMatrix() {
   for (let i = 0; i < MATRIX_HEIGHT; i++) {
     res[i] = [];
     for (let j = 0; j < MATRIX_WIDTH; j++) {
-      if (i < 12) {
+      if (i < 13) {
         //dirt
         res[i][j] = 0;
+      } else if (i == 13) {
+        res[i][j] = 2;
       } else {
         res[i][j] = 1;
       }
@@ -51,6 +54,9 @@ function drawBoard(boardMatrix, boardContainer) {
         case 1:
           newTile.classList.add(textures.dirt);
           break;
+        case 2:
+          newTile.classList.add(textures.grass);
+
         default:
           newTile.classList.add(textures.sky);
           break;
@@ -70,4 +76,5 @@ function selectTool(e) {
     t.classList.remove("selected");
   });
   e.currentTarget.classList.add("selected");
+  console.log("selected ", e.currentTarget.getAttribute("data-toolType"));
 }
